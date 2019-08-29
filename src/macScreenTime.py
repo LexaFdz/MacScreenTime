7eb9ecb6f013aaa68ddbd4ded870c80815993e7d
Alexander Fernandez <alexander.fada@gmail.com>
2019-08-29 13:31:25 -0400

    Now stores and shows time spent on each app

--- src/macScreenTime.py
+++ src/macScreenTime.py
@@ -1,8 +1,8 @@
 # Requiere ---> pip3 install PyObjC
 import time
 import json
+from datetime import datetime
 from AppKit import NSWorkspace
-
 
 """
 Variables iniciales
@@ -11,7 +11,8 @@ Variables iniciales
 
 def serialize():
     return {
-        'app_name': get_active_app_name()
+        'app_name': previous_app_name,
+        'date': str(time_on_app)
     }
 
 
@@ -39,15 +40,31 @@ try:
     Si tenia una app anterior salvada, se compara y salva si cambia.
     """
     app_list = []
-    previous_app_name = 'app inventada'
+    start_time = ''
+    previous_app_name = 'algo inventado'
+
     while True:
 
-        new_app_name = serialize()
+        current_time = datetime.now()
+        new_app_name = get_active_app_name()
         # if 'Google Chrome' in new_app_name:
         #     new_app_name = url_to_name()
+
         if previous_app_name != new_app_name:
-            print(new_app_name)
-            app_list.append(new_app_name)
+            if start_time == '':
+                start_time = datetime.now()
+
+            '''
+            Calculamos el tiempo que duro en la app que este de focus.
+            '''
+            time_on_app = current_time-start_time
+            print(previous_app_name)
+            print(str(time_on_app))
+
+            app_list.append(serialize())
+
+            start_time = current_time
+            previous_app_name = new_app_name
             # activity_name = active_app_name
 
             # if not first_time:
@@ -55,7 +72,6 @@ try:
             # with open('apps.json', 'w') as json_file:
             #     json.dump()
 
-            previous_app_name = new_app_name
             """
             Escribo en un archivo la lectura constante de la app en focus
             """
@@ -67,4 +83,3 @@ try:
 
 except KeyboardInterrupt:
     raise
-
